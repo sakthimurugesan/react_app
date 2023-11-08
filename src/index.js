786a926e514f50c95601fff23bc31037c5a09511
SAKTHI M <msakthi150@gmail.com>
2023-11-08 10:11:16 +0530

    loading images

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-function PassMessage()
-{
-  return(
-    <h1 style={
-      {
-        color:'green',
-        fontWeight:'bolder'
-      }
-    }>You are Pass</h1>
-    );
-  }
-  function FailMessage()
-  {
-    return(
-      <h1 style={
-        {
-        color:'green',
-        fontWeight:'bolder'
-      }
-    }>You are Pass</h1>
-  );
-}
-var marks=50;
 function Display()
 {
-  if(marks>=50)
   return(
-<PassMessage></PassMessage>
-);
-  else
-  return(
-<FailMessage></FailMessage>
-);
+    <div>
+    <img src="image/ant.jpg" width={500} height={500} alt="antImage"></img>   
+    </div>
+  );
 }
 ReactDom.render(<Display></Display>,document.getElementById("main"));
